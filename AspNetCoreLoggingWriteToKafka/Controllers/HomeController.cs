51d81f353a9e89abc8072d5ed0724da11e640582
bidianqing <bidianqing@longfor.com>
2023-06-09 17:31:20 +0800

    Add project files.

---
+++ AspNetCoreLoggingWriteToKafka/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreLoggingWriteToKafka.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("test");
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ AspNetCoreLoggingWriteToKafka/Program.cs
@@ -0,0 +1,28 @@
+using Serilog;
+using Serilog.Sinks.Kafka;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson();
+
+builder.Logging.ClearProviders();
+
+var logger = new LoggerConfiguration()
+    //.ReadFrom.Configuration(builder.Configuration)
+    .WriteTo.Kafka()
+    .CreateLogger();
+builder.Logging.AddSerilog(logger);
+builder.Logging.AddConsole();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
